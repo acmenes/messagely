94274993935035786c2979805abbfc4980f5d173
acmenes <acmenescompositions@gmail.com>
2021-08-28 16:27:54 -0400

    routes for auth and messages

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 const Router = require("express").Router;
 const router = new Router();
 
+const User = require("../models/user")
+
 const {SECRET_KEY} = require("../config");
 const ExpressError = require("../expressError");
 
@@ -11,6 +13,22 @@ const ExpressError = require("../expressError");
  *
  **/
 
+router.post("/login", async function (req, res, next){
+    try {
+        let { username, password } = req.body;
+        if (await User.authenticate(username, password)) {
+            let token = jwt.sign({username}, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({token});
+        } else {
+            throw new ExpressError("Invalid username and/or passowrd", 400)
+        }
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -18,5 +36,17 @@ const ExpressError = require("../expressError");
  *
  *  Make sure to update their last-login!
  */
+
+
+router.post("/register", async function (req, res, next){
+    try {
+        let {username} = await User.register(req.body);
+        let token = jwt.sign({username}, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({token})
+    }catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,9 @@
 const Router = require("express").Router;
 const router = new Router();
 
-
+const Message = require("../models/message");
+const {ensureLoggedIn} = require("../middleware/auth");
+const expressError = require("../expressError");
 
 /** GET /:id - get detail of message.
  *
@@ -16,6 +18,21 @@ const router = new Router();
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res, next){
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
+
+        if(msg.to_user.username !== username && msg.from_user.username !== username){
+            throw new ExpressError("You cannot read this message", 401)
+        }
+    return res.json({ message: msg })
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
 
 /** POST / - post message.
  *
@@ -23,6 +40,38 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+
+ router.post("/", ensureLoggedIn, async function (req, res, next){
+    try {
+        let msg = await Message.create({
+            from_username = req.user.username,
+            to_username = req.body.to_username,
+            body: req.body.body
+        });
+    return res.json({ message: msg })
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next){
+    try {
+        let username = req.user.username
+        let msg = await Message.get(req.params.id)
+
+        if (msg.to_username !== username) {
+            throw new expressError ("Cannot set this message to read", 401)
+        }
+
+        let message = await Message.markRead(req.params.id);
+
+        return res.json({message});
+    }
+    catch (err) {
+        return next(err);
+    }
+});
 
 
 /** POST/:id/read - mark message as read:
